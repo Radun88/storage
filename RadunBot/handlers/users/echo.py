0f7f3ef875a345684b6dcf8474ef84ff63cb66a4
Sergey <s.svistunov@hotmail.com>
2021-03-09 22:57:38 +0300

    Обновление

--- RadunBot/handlers/users/echo.py
+++ RadunBot/handlers/users/echo.py
@@ -1,7 +1,23 @@
-from aiogram import types
 from loader import dp
+from aiogram import types
+
+
+@dp.message_handler(text='/start')
+async def bot_echo(message: types.Message, state):
+    chat_id = message.chat.id
+    text = message.text
+    await bot.send_message(chat_id=chat_id, text=text)
+    await message.answer(text)
+    await message.reply(text)
+
+
+
 
 
 @dp.message_handler()
-async def bot_echo(message: types.Message):
-    await message.answer(message.text)
+async def bot_echo(message: types.Message, state):
+    chat_id = message.chat.id
+    text = message.text
+    await bot.send_message(chat_id=chat_id, text=text)
+    await message.answer(text)
+    await message.reply(text)
